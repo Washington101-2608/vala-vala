02a2680f90da9746886cbeb2e66997213ac2ae4f
Washington101-2608 <washingtonnyirongo1@gmail.com>
2026-05-31 19:48:57 +0200

    fix: clean up serializers

--- core/serializers.py
+++ core/serializers.py
@@ -9,22 +9,6 @@ class ProductSerializer(serializers.ModelSerializer):
         exclude = ['date_created']
 
     def get_image(self, obj):
-        request = self.context.get('request')
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
+        if obj.image:
+            return obj.image.url
         return ''
-
-    from rest_framework import serializers
-    from core.models import Product
-
-    class ProductSerializer(serializers.ModelSerializer):
-        image = serializers.SerializerMethodField()
-
-        class Meta:
-            model = Product
-            exclude = ['date_created']
-
-        def get_image(self, obj):
-            if obj.image:
-                return obj.image.url  # Cloudinary already returns full URL
-            return ''
