7109fc1ccb32a63e1cd1742913d2e30d945dda79
Washington101-2608 <washingtonnyirongo1@gmail.com>
2026-05-31 19:03:13 +0200

    fix: use cloudinary URL for images

--- core/serializers.py
+++ core/serializers.py
@@ -13,3 +13,18 @@ class ProductSerializer(serializers.ModelSerializer):
         if obj.image and request:
             return request.build_absolute_uri(obj.image.url)
         return ''
+
+    from rest_framework import serializers
+    from core.models import Product
+
+    class ProductSerializer(serializers.ModelSerializer):
+        image = serializers.SerializerMethodField()
+
+        class Meta:
+            model = Product
+            exclude = ['date_created']
+
+        def get_image(self, obj):
+            if obj.image:
+                return obj.image.url  # Cloudinary already returns full URL
+            return ''
