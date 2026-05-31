130b128e86f3b3ca980e2b971f1492decaf49560
Washington101-2608 <washingtonnyirongo1@gmail.com>
2026-05-31 19:40:12 +0200

    debug: check image URL

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('api/products/<int:prod_id>/',views.UpdateProduct.as_view(),name='update_product'),
     path('api/dashboard/',views.Dashboard,name='dashboard'),
     path('api/login/',views.LoginView.as_view(),name='login'),
-    path("api/product/dashboard",views.DasbboardView.as_view,name='api_dashboard')
+    path("api/product/dashboard",views.DasbboardView.as_view,name='api_dashboard'),
 path('api/debug/', views.DebugView.as_view(), name='debug'),
 ]
